11b2dcbe85a6a06660c1514eb2128d2e5f300bac
Pushpinder Rana <er.pushpinderrana@gmail.com>
2022-01-31 13:01:54 -0800

    Update CustomNestedParagraph.php

--- src/Plugin/migrate/process/CustomNestedParagraph.php
+++ src/Plugin/migrate/process/CustomNestedParagraph.php
@@ -9,7 +9,8 @@ use Drupal\migrate\Row;
 /**
  * Runs an array of arrays through its own process pipeline.
  *
- *
+ * First, I tried the OOTB sub_process plugin but it couldn't work in this scenario.
+ * 
  * @MigrateProcessPlugin(
  *   id = "custom_nested_paragraph",
  *   handle_multiples = TRUE
@@ -22,7 +23,25 @@ class CustomNestedParagraph extends ProcessPluginBase {
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     $return = [];
-
+    // The recommendation is to use the OOTB sub_process plugin.
+    // Since it didn't work in this case so I developed this custom plugin.
+    // After triaging what expected in the Nested Paragraph field in Article node
+    // i.e. field_article_nested_items, it was easy to write this plugin.
+    // The expected array was something like below:
+    // array:2 [
+    //   0 => array:2 [
+    //     "target_id" => "32241"
+    //     "target_revision_id" => "3031011"
+    //   ]
+    //   1 => array:2 [
+    //     "target_id" => "32242"
+    //     "target_revision_id" => "3031012"
+    //   ]
+    // ]
+    // Hence, iterates all the paragraph items and get the source &
+    // destination id. After iterating through all the paragraphs 
+    // return the final nested paragraph array.
+    
     $nested_paragraphs_items = [
       'migrate_nested_paragraphs_banner_content',
       'migrate_nested_paragraphs_hero_content',
